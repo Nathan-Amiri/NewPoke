2c0f5a00589fd8b32bd5edfb5495dc1a1d7ce7e9
nathan-amiri <nmort17@aol.com>
2025-03-26 14:39:22 -0500

    added cpu effective move seeds

--- Assets/Scripts/CPU.cs
+++ Assets/Scripts/CPU.cs
@@ -11,16 +11,39 @@ public class CPU : MonoBehaviour
 
     public List<ChoiceInfo> GetCPUChoices()
     {
-        int ally1AdvantageIntoEnemy1 = GetMoveAdvantage(gameManager.pokemonSlots[2], gameManager.pokemonSlots[0]);
-        int ally1AdvantageIntoEnemy2 = GetMoveAdvantage(gameManager.pokemonSlots[2], gameManager.pokemonSlots[1]);
+        AddMostEffectiveMoveSeeds(gameManager.pokemonSlots[2], gameManager.pokemonSlots[0]);
+        AddMostEffectiveMoveSeeds(gameManager.pokemonSlots[2], gameManager.pokemonSlots[1]);
 
         return null;
     }
-    private int GetMoveAdvantage(PokemonSlot caster, PokemonSlot target)
+    private void AddMostEffectiveMoveSeeds(PokemonSlot ally, PokemonSlot enemy)
     {
-        // Cache the most effective move caster has into target. Advantage = effectiveness (0/1/2/4/6/8)
+        // Cache the most effective move ally has into enemy, set advantage equal to effectiveness
+        (MoveData, int) allyMoveIntoEnemy = GetMoveAdvantage(ally, enemy);
 
-        MoveData cachedMove;
+        // Cache the most effective move enemy has into ally, subtract advantage based on effectiveness
+        (MoveData, int) enemyMoveIntoAlly = GetMoveAdvantage(enemy, ally);
+        allyMoveIntoEnemy.Item2 -= enemyMoveIntoAlly.Item2;
+
+        // Add/subtract effectiveness based on speed difference
+        allyMoveIntoEnemy.Item2 += GetSpeedAdvantage(ally, enemy);
+
+        // Add most effective moves into both enemies to hat
+        ChoiceInfo info = new()
+        {
+            casterName = ally.data.pokemonName,
+            casterSlot = ally,
+            move = allyMoveIntoEnemy.Item1,
+            targetSlot = allyMoveIntoEnemy.Item1.isTargeted ? enemy : null
+        };
+        for (int i = 0; i < allyMoveIntoEnemy.Item2; i++)
+            hat.Add(info);
+    }
+    private (MoveData, int) GetMoveAdvantage(PokemonSlot caster, PokemonSlot target)
+    {
+        // Returns the most effective move into the target, and its advantage (Advantage = effectiveness(0 / 1 / 2 / 4 / 6 / 8))
+
+        MoveData cachedMove = default;
         float greatestEffectiveness = 0;
         foreach (MoveData move in caster.data.moves)
         {
@@ -36,9 +59,42 @@ public class CPU : MonoBehaviour
             }
         }
 
+        int advantage;
+        if (greatestEffectiveness == 0)
+            advantage = 0;
+        else if (greatestEffectiveness == .25f)
+            advantage = 1;
+        else if (greatestEffectiveness == .5f)
+            advantage = 2;
+        else if (greatestEffectiveness == 1)
+            advantage = 4;
+        else if (greatestEffectiveness == 2)
+            advantage = 6;
+        else // greatestEffectiveness == 4
+            advantage = 8;
 
+        return (cachedMove, advantage);
+    }
 
+    private int GetSpeedAdvantage(PokemonSlot ally, PokemonSlot enemy)
+    {
+        if (ally.data.currentSpeed > enemy.data.currentSpeed)
+        {
+            if (ally.data.currentAttack < enemy.data.currentHealth)
+                return 1;
 
+            return 3;
+        }
+
+        if (ally.data.currentSpeed < enemy.data.currentSpeed)
+        {
+            if (enemy.data.currentAttack < ally.data.currentHealth)
+                return -1;
+
+            return -3;
+        }
+
+        // Speeds are equal
         return 0;
     }
 }
